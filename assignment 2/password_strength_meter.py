b286dab8aa69fdc22e6156e089de00e7548fe81e
Mahnoor Ashfaq <159866653+mahnoorashfaq@users.noreply.github.com>
2025-04-15 12:32:58 +0500

    Add files via upload

--- assignment 2/password_strength_meter.py
+++ assignment 2/password_strength_meter.py
@@ -3,36 +3,41 @@ import streamlit as st
 
 st.title("🔐 Password Strength Meter")
 
-def check_password(password):
-    score = 0
+password = st.text_input ("Enter Your Text" , type= "password")
+
+feedback= []
+score = 0
+
+if password:
 
     if len(password) >= 8:
         score += 1
     else:
-        print("❌ Password should be at least 8 characters long.")
+        feedback.append("❌ Password should be at least 8 characters long.")
 
     if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
         score += 1
     else:
-        print("❌ Include both uppercase and lowercase letters.")
+        feedback.append("❌ Include both uppercase and lowercase letters.")
 
     if re.search(r"\d", password):
         score += 1
     else:
-        print("❌ Add at least one number (0-9).")
+        feedback.append("❌ Add at least one number (0-9).")
 
     if re.search(r"[!@#$%^&*]", password):
         score += 1
     else:
-        print("❌ Include at least one special character (!@#$%^&*).")
+        feedback.append("❌ Include at least one special character (!@#$%^&*).")
     
     if score == 4:
-        print("✅ Strong Password!")
+        feedback.append("✅ Strong Password!")
     elif score == 3:
-        print("⚠️ Moderate Password - Consider adding more security features.")
+        feedback.append("⚠️ Moderate Password - Consider adding more security features.")
     else:
-        print("❌ Weak Password - Improve it using the suggestions above.")
+        feedback.append("❌ Weak Password - Improve it using the suggestions above.")
 
-password = input("Enter your password: ")
-
-check_password(password)
+    if feedback :
+        st.markdown ("Improvement Suggestion")
+        for tip in feedback :
+            st.write(tip)
